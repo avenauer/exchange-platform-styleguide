270ebdb2cde28123e8d85b7744ff14583c19776c
MARCIN-PC\Marcin <nalkez69@gmail.com>
2021-05-10 11:18:32 +0200

    storybook static

---
+++ storybook-static/main.6ad460094d47eafa5ecd.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(446),c(586),n.exports=c(995)},1:function(n,o){},586:function(n,o,c){"use strict";c.r(o);c(587),c(909),c(966),c(970)}},[[0,1,2]]]);
